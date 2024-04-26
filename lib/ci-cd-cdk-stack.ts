1f90a7f2d6adb8ea359d9bb4c503dff01ecb6103
satheeshcm <cmsatheesh@gmail.com>
2024-04-26 16:32:37 +0530

    v1

--- lib/ci-cd-cdk-stack.ts
+++ lib/ci-cd-cdk-stack.ts
@@ -1,22 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
-import { CodePipeline } from 'aws-cdk-lib/aws-events-targets';
-import { CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
-export class CiCdCdkStack extends cdk.Stack {
+export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'pipeline', {
-      piplineName: 'Test Pipeline' 
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('satheeshcm/ci-cd-cdk', 'main'),
-        commands:['npm ci',
-                  'npm run build',
-                  'npx cdk synth']
+        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
-    })
-    
+    });
   }
 }
+      })
+    })
+ 
+    }  }
+
